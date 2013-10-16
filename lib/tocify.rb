f3e08ea2b65866b4305a9b03bfab7081de62451d
pyro2927 <joseph@pintozzi.com>
2013-10-16 16:51:46 -0500

    Reading lines instead of regex

--- lib/tocify.rb
+++ lib/tocify.rb
@@ -58,8 +58,21 @@ module Tocify
   end
 
   def self.insert(toc, file_name)
-    content = File.read(file_name)
-    content.sub!(/(\n## Table of Contents.+|)\n##/im, "\n" + toc + "\n\n##")
-    File.write(file_name, content)
+    lines = File.read_lines(file_name)
+    existing_toc = false
+    header = toc.split("\n")[0]
+    toc_index = 0
+    lines.each do |line|
+      if line.start_with? header
+        existing_toc = true
+      elsif line.start_with? "##"
+        toc_index = lines.index(line)
+        break
+      end
+
+      lines.delete(line) if existing_toc
+    end
+    lines.insert(toc_index, [""] + toc.split("\n") + ["",""])
+    File.write(file_name, lines.join("\n"))
   end
 end
